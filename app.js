85a3c83be61f12cca116da597cd859437d033c7d
Fabio Rogerio <fabio.rogerio@tecnospeed.local@tspd-nb-00411.tecnospeed.local>
2019-03-28 16:26:17 -0300

    fix audit npm

--- app.js
+++ app.js
@@ -1,78 +1,80 @@
-var express = require('express');
-var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-var bodyParser = require('body-parser');
-var fs = require('fs');
-var path = require("path");
-var os = require('os');
-var exec = require('child_process').exec;
-var child;
-const imagemin = require('imagemin');
-const imageminMozjpeg = require('imagemin-mozjpeg');
-const imageminPngquant = require('imagemin-pngquant');
+var express = require('express')
+var app = express()
+var server = require('http').Server(app)
+var io = require('socket.io')(server)
+var bodyParser = require('body-parser')
+var fs = require('fs')
+var path = require('path')
+var os = require('os')
+var exec = require('child_process').exec
+var child
+const imagemin = require('imagemin')
+const imageminMozjpeg = require('imagemin-mozjpeg')
+const imageminPngquant = require('imagemin-pngquant')
 const util = require('./util')
 
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 var port = process.argv[2]
 var timeGetScreen = process.argv[3]
 
-function getScreen(){
-  //var cmd = __dirname+'/nircmd.fbi savescreenshot www/images/screen.png';
-  var cmd = __dirname+'/cmdcapture.fbi /f screen.png';
-  child = exec(cmd,
-    function (error, stdout, stderr) {
+function getScreen () {
+  if (process.platform !== 'linux') {
+    var cmd = __dirname + '/cmdcapture.fbi /f screen.png'
+    child = exec(cmd, function (error, stdout, stderr) {
       imagemin(['./screen.png'], './www/images', {
-          plugins: [
-              imageminMozjpeg({targa: true}),
-              imageminPngquant({quality: '65-80'})
-          ]
+        plugins: [
+          imageminMozjpeg({ targa: true }),
+          imageminPngquant({ quality: '65-80' })
+        ]
       }).then(files => {
-          io.sockets.emit('showscreen');
-          setTimeout(getScreen, timeGetScreen);
-      });
-  });
+        io.sockets.emit('showscreen')
+        setTimeout(getScreen, timeGetScreen)
+      })
+    })
+  }
 }
 
-getScreen();
+getScreen()
 
-//Socket
-io.on('connection', function (socket) {});
+// Socket
+io.on('connection', function (socket) {})
+
+if (!fs.existsSync(path.join(__dirname, 'www', 'downloads'))) {
+  fs.mkdirSync(path.join(__dirname, 'www', 'downloads'))
+}
 
 fs.watch(path.join(__dirname, 'www', 'downloads'), function (event, filename) {
-  io.sockets.emit('filechangedownload', { });
-});
+  io.sockets.emit('filechangedownload', { })
+})
 
-process.setMaxListeners(0);
-var FRONTEND_PATH = __dirname+'/www';
-app.use(express.static(FRONTEND_PATH));
+process.setMaxListeners(0)
+var FRONTEND_PATH = __dirname + '/www'
+app.use(express.static(FRONTEND_PATH))
 
-app.get('/', function(req, res) {
-	res.sendfile(FRONTEND_PATH+'/index.html');
-});
-app.post('/getfilesdownload', function(req, res){
-  res.jsonp(util.dirTree(path.join(__dirname, 'www', 'downloads'), true));
-});
-app.post('/getfile', function(req, res){
-	if(req.body.file){
-		res.sendfile(req.body.file);
-	} else {
-		res.send("");
-	}
-});
-app.get('/getfile', function(req, res){
-  if(req.query.file){
-    res.sendfile(req.query.file);
-  } else if(req.query.screen) {
-    res.sendfile(FRONTEND_PATH + "/images/screen.png");
+app.get('/', function (req, res) {
+  res.sendfile(FRONTEND_PATH + '/index.html')
+})
+app.post('/getfilesdownload', function (req, res) {
+  res.jsonp(util.dirTree(path.join(__dirname, 'www', 'downloads'), true))
+})
+app.post('/getfile', function (req, res) {
+  if (req.body.file) {
+    res.sendfile(req.body.file)
   } else {
-    res.send("");  
+    res.send('')
   }
-});
+})
+app.get('/getfile', function (req, res) {
+  if (req.query.file) {
+    res.sendfile(req.query.file)
+  } else if (req.query.screen) {
+    res.sendfile(FRONTEND_PATH + '/images/screen.png')
+  } else {
+    res.send('')
+  }
+})
 
-server.listen(port);
-console.log('ShowMyScreen running: ' + port);
-
+server.listen(port)
+console.log('ShowMyScreen running: ' + port)
--- js/script.js
+++ js/script.js
@@ -1,187 +1,187 @@
-const {dialog} = require('electron').remote
+const { dialog } = require('electron').remote
 const { spawn } = require('child_process')
 const os = require('os')
 const fs = require('fs')
 const path = require('path')
 const util = require('./util')
 
-function showForm() {
-    document.getElementById('buttons-init').style.display='none'
-    document.getElementById('form-settings').style.display='block'
-    var port = localStorage.getItem('port')
-    var fps = localStorage.getItem('fps')
-    if(fps) {
-        fps = 1000 / Number(fps)
-        document.getElementById('fps').value = fps
+function showForm () {
+  document.getElementById('buttons-init').style.display = 'none'
+  document.getElementById('form-settings').style.display = 'block'
+  var port = localStorage.getItem('port')
+  var fps = localStorage.getItem('fps')
+  if (fps) {
+    fps = 1000 / Number(fps)
+    document.getElementById('fps').value = fps
+  }
+  if (port) {
+    document.getElementById('port').value = port
+  }
+}
+
+function hideForm () {
+  document.getElementById('form-settings').style.display = 'none'
+  document.getElementById('buttons-init').style.display = 'block'
+}
+
+function save () {
+  var port = document.getElementById('port').value
+  var fps = document.getElementById('fps').value
+  if (fps) fps = 1000 / Number(fps)
+  localStorage.setItem('port', port)
+  localStorage.setItem('fps', fps)
+  hideForm()
+}
+
+function start () {
+  var port = 80
+  if (localStorage.getItem('port')) port = localStorage.getItem('port')
+  util.isPortTaken(port, (err, res) => {
+    console.log(err, res)
+    if (err) {
+      return dialog.showMessageBox({
+        type: 'error',
+        title: 'Error',
+        message: `Could not start application on port: ${port}`,
+        detail: JSON.stringify(err)
+      })
     }
-    if(port) {
-        document.getElementById('port').value = port
-    }
+
+    runApp(port)
+  })
 }
 
-function hideForm() {
-    document.getElementById('form-settings').style.display='none'
-    document.getElementById('buttons-init').style.display='block'
-}
-
-function save() {
-    var port = document.getElementById('port').value
-    var fps = document.getElementById('fps').value
-    if(fps) fps = 1000 / Number(fps)
-    localStorage.setItem('port', port)
-    localStorage.setItem('fps', fps)
-    hideForm()
-}
-
-function start() {
-    var port = 80
-    if(localStorage.getItem('port')) port = localStorage.getItem('port')
-    util.isPortTaken(port, (err, res) => {
-        console.log(err, res)
-        if(err) {
-            return dialog.showMessageBox({
-                type: 'error',
-                title: 'Error',
-                message: `Could not start application on port: ${port}`,
-                detail: JSON.stringify(err)
-            })
-        }
-
-        runApp(port)
-    })
-}
-
-function stop() {
-    document.body.classList = ''
-    runShowMyScreen.kill()
+function stop () {
+  document.body.classList = ''
+  runShowMyScreen.kill()
 }
 
 var runShowMyScreen = null
 
-function runApp(port) {
-    var fps = 50
-    if(localStorage.getItem('fps')) fps = localStorage.getItem('fps')
-    runShowMyScreen = spawn('node', ['app.js',port, fps], {
-        cwd: __dirname
-    })
-    runShowMyScreen.stdout.on('data', (data) => {
-        console.log(`stdout: ${data}`)
-        if(data.indexOf('running') >= 0) {
-            document.body.classList = 'started'
-            
-            getIPS((ips) => {
-                console.log(ips)
-                document.getElementById('show-ips').innerHTML = 'OPEN THE BROWSER WITH ONE OF THE IPs BELOW:\n\n'
-                for(o of ips) {
-                    document.getElementById('show-ips').innerHTML += `\nHOST: http://${o.ip}${port!=80?':'+port:''}`
-                    document.getElementById('show-ips').innerHTML += `\n(${o.name})\n`
-                }
-            })
+function runApp (port) {
+  var fps = 50
+  if (localStorage.getItem('fps')) fps = localStorage.getItem('fps')
+  runShowMyScreen = spawn('node', ['app.js', port, fps], {
+    cwd: __dirname
+  })
+  runShowMyScreen.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`)
+    if (data.indexOf('running') >= 0) {
+      document.body.classList = 'started'
 
-            getFilesDownloads()
-
-            dropZone()
+      getIPS((ips) => {
+        console.log(ips)
+        document.getElementById('show-ips').innerHTML = 'OPEN THE BROWSER WITH ONE OF THE IPs BELOW:\n\n'
+        for (o of ips) {
+          document.getElementById('show-ips').innerHTML += `\nHOST: http://${o.ip}${port != 80 ? ':' + port : ''}`
+          document.getElementById('show-ips').innerHTML += `\n(${o.name})\n`
         }
-    })
+      })
 
-    runShowMyScreen.stderr.on('data', (data) => {
-        dialog.showMessageBox({
-            type: 'error',
-            title: 'Error',
-            message: `Could not start application on port: ${port}`,
-            detail: data.toString()
-        })
-    })
+      getFilesDownloads()
 
-    runShowMyScreen.on('close', (code) => {
-        console.log(`child process exited with code ${code}`)
-    })   
+      dropZone()
+    }
+  })
+
+  runShowMyScreen.stderr.on('data', (data) => {
+    dialog.showMessageBox({
+      type: 'error',
+      title: 'Error',
+      message: `Could not start application on port: ${port}`,
+      detail: data.toString()
+    })
+  })
+
+  runShowMyScreen.on('close', (code) => {
+    console.log(`child process exited with code ${code}`)
+  })
 }
 
-function getFilesDownloads() {
-    var files = util.dirTree(path.join(__dirname, 'www', 'downloads'), true)
-    var list = document.getElementById('list')
-    list.innerHTML = ''
-    for(o of files.children) {
-        if(o.type === 'file') {
-            list.innerHTML += `
+function getFilesDownloads () {
+  var files = util.dirTree(path.join(__dirname, 'www', 'downloads'), true)
+  var list = document.getElementById('list')
+  list.innerHTML = ''
+  for (o of files.children) {
+    if (o.type === 'file') {
+      list.innerHTML += `
             <div class="item">
                 <img onclick="removeFile('${o.path}')" src="img/waste-bin.png" title="Remove">
                 ${o.name}
             </div>
             `
-        }
     }
+  }
 }
 
 function removeFile (file) {
-    var fileName = file.split('downloads')[1]
-    fileName = fileName.replace('\\', '')
-    fileName = fileName.replace('/', '')
-    var p = path.join(__dirname, 'www', 'downloads', fileName)
-    fs.unlinkSync(p)
-    getFilesDownloads()
+  var fileName = file.split('downloads')[1]
+  fileName = fileName.replace('\\', '')
+  fileName = fileName.replace('/', '')
+  var p = path.join(__dirname, 'www', 'downloads', fileName)
+  fs.unlinkSync(p)
+  getFilesDownloads()
 }
 
 function getIPS (callback) {
-    var ifaces = os.networkInterfaces()
-    var ips = []
-    Object.keys(ifaces).forEach(function (ifname) {
-      var alias = 0
-    
-      ifaces[ifname].forEach(function (iface) {
-        if ('IPv4' !== iface.family || iface.internal !== false) {
-          return
-        }
-    
-        if (alias >= 1) {
-            ips.push({
-                name: ifname,
-                ip: iface.address
-            })
-        } else {
-            ips.push({
-                name: ifname,
-                ip: iface.address
-            })
-        }
-      })
+  var ifaces = os.networkInterfaces()
+  var ips = []
+  Object.keys(ifaces).forEach(function (ifname) {
+    var alias = 0
+
+    ifaces[ifname].forEach(function (iface) {
+      if (iface.family !== 'IPv4' || iface.internal !== false) {
+        return
+      }
+
+      if (alias >= 1) {
+        ips.push({
+          name: ifname,
+          ip: iface.address
+        })
+      } else {
+        ips.push({
+          name: ifname,
+          ip: iface.address
+        })
+      }
     })
-    callback(ips)
+  })
+  callback(ips)
 }
 
-function shareFile() {
-    dialog.showOpenDialog({
-        title: 'Share file',
-        filters: [{name: 'All Files', extensions: ['*']}]
-    }, (data) => {
-        for(d of data){
-            var name = path.basename(d)
-            var fileTo = path.join(__dirname, 'www', 'downloads', name)
-            fs.createReadStream(d).pipe(fs.createWriteStream(fileTo))
-        }
-        getFilesDownloads()
-    })
-}
-
-function dropZone() {
-    var holder = document.getElementById('dropZone');
-    
-    holder.ondragover = () => false
-
-    holder.ondragleave = () => false
-
-    holder.ondragend = () => false
-
-    holder.ondrop = (e) => {
-        e.preventDefault()
-
-        for (let f of e.dataTransfer.files) {
-            var name = path.basename(f.path)
-            var fileTo = path.join(__dirname, 'www', 'downloads', name)
-            fs.createReadStream(f.path).pipe(fs.createWriteStream(fileTo))
-        }
-        getFilesDownloads()
-        return false
+function shareFile () {
+  dialog.showOpenDialog({
+    title: 'Share file',
+    filters: [{ name: 'All Files', extensions: ['*'] }]
+  }, (data) => {
+    for (d of data) {
+      var name = path.basename(d)
+      var fileTo = path.join(__dirname, 'www', 'downloads', name)
+      fs.createReadStream(d).pipe(fs.createWriteStream(fileTo))
     }
+    getFilesDownloads()
+  })
+}
+
+function dropZone () {
+  var holder = document.getElementById('dropZone')
+
+  holder.ondragover = () => false
+
+  holder.ondragleave = () => false
+
+  holder.ondragend = () => false
+
+  holder.ondrop = (e) => {
+    e.preventDefault()
+
+    for (let f of e.dataTransfer.files) {
+      var name = path.basename(f.path)
+      var fileTo = path.join(__dirname, 'www', 'downloads', name)
+      fs.createReadStream(f.path).pipe(fs.createWriteStream(fileTo))
+    }
+    getFilesDownloads()
+    return false
+  }
 }
